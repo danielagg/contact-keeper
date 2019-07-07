f5b621b2e79ab197a50a4dd2ae545aad9fa9f6d8
Daniel Agg <daniel.agg@outlook.com>
2019-07-07 15:12:46 +0200

    Saving users to DB

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
+const bcrypt = require("bcryptjs");
 const router = express.Router();
-const { check, validationResult } = require("express-validator/check");
+// const { check, validationResult } = require("express-validator/check");
+const { check, validationResult } = require("express-validator");
 
 const User = require("../models/User");
 
@@ -17,13 +19,36 @@ router.post(
       "Please enter a password with 6 or more characters"
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
+    // check validation first
     const errors = validationResult(req);
+
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send("passed");
+    const { name, email, password } = req.body;
+    try {
+      // check whether user already exists in DB
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res.status(400).json({ msg: "User already exists" });
+      }
+
+      // save new user with hashed password
+      user = User({ name, email, password });
+
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      res.send("todo: User saved");
+    } catch (ex) {
+      console.error(ex.message);
+      res.status(500).json({ msg: "Could not create user" });
+    }
   }
 );
 
