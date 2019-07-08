5271e2a6b4bbb28a621ddcafd7916abd0eb183b7
Daniel Agg <daniel.agg@outlook.com>
2019-07-08 13:12:18 +0200

    Setting up basic react-router, created skeletons for Home, About components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,25 @@
 import React from "react";
-import Navbar from "./components/layout/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
-function App() {
+// Components
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+
+const App = () => {
   return (
-    <div className="App">
-      <Navbar />
-      <h1>Cleaned up</h1>
-    </div>
+    <Router>
+      <>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/about" component={About} />
+          </Switch>
+        </div>
+      </>
+    </Router>
   );
-}
+};
 
 export default App;
