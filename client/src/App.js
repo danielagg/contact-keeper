252c06f5756ca672b3606e5b76984a976f2bc710
Daniel Agg <daniel.agg@outlook.com>
2019-07-09 11:35:26 +0200

    Added auth state via Context API

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+// Context
 import ContactState from "./context/contact/ContactState";
+import AuthState from "./context/auth/AuthState";
+
 // Components
 import Navbar from "./components/layout/Navbar";
 import Home from "./components/pages/Home";
@@ -9,19 +12,21 @@ import About from "./components/pages/About";
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path="/" component={Home} />
-              <Route exact path="/about" component={About} />
-            </Switch>
-          </div>
-        </>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
+          </>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 };
 
