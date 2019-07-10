b434960858448d31395985be9fbb5233272c4825
Daniel Agg <daniel.agg@outlook.com>
2019-07-10 14:27:54 +0200

    Preparing for Heroku deploy

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDb = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -14,6 +15,11 @@ app.use("/api/auth", require("./routes/auth"));
 app.use("/api/users", require("./routes/users"));
 app.use("/api/contacts", require("./routes/contacts"));
 
-app.get("/", (req, res) => res.json({}));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
 
 app.listen(PORT, () => console.log(`The server has started on port ${PORT}`));
